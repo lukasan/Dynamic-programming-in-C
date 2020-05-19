ba472b7efa7513a106d3a3b2b5f13851e359580e
Lukas <luk.antanavicius@gmail.com>
2020-05-19 18:46:54 +0300

    Working find minimum recursion

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # Interval dynamic programming in C
 Pages 22 to 26 inclusive: https://web.stanford.edu/class/cs97si/04-dynamic-programming.pdf
 
+Making a polindrome out of a given string inserting least characters possible.
+
 Implementing expression:
 
 ![Image description](dyn.png)
--- interval-dp.c
+++ interval-dp.c
@@ -1,10 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+int findMin(int a, int b){
+    if (a > b) return b;
+    else return a;
+}
+
+int findMinInserts(char *str, int first, int last){
+    // Base cases
+    if (first == last)
+        return 0;
+    if (first > last){
+        printf("Something went wrong...\n");
+        abort();
+    }
+    if (first == last - 1){
+        if (str[first] == str[last])
+            return 1;
+        else 
+            return 0;
+    }
+    
+    // Main case
+    if (str[first] == str[last]){
+        return findMinInserts(str, first + 1, last - 1);
+    }
+    else {
+        return findMin(
+            findMinInserts(str, first, last - 1),
+            findMinInserts(str, first + 1, last)
+        ) + 1;
+    }
+}
 
 int main(int argc, char **argv){
-    if (argc != 3){
-        printf("Usage: %s firstString secondString\n", __FILE__);
+    if (argc != 2){
+        printf("Usage: %s inputString\n", __FILE__);
         exit(255);
     }
+
+    printf("%d\n", findMinInserts(argv[1], 0, strlen(argv[1]) - 1));
     return 0;
 }
